16e2e7c9fdc4183228973da7f8a2d58fe965def6
Rishav Ghosh <rishavghosh@Rishavs-MacBook-Air.local>
2026-09-09 08:57:11 +0530

    updated main.py

--- Text to SQL Converter/main.py
+++ Text to SQL Converter/main.py
@@ -13,14 +13,18 @@ while True:
     user_input=input("Ask your Qustion: ")
     if user_input.lower()=='exit':
         break
-    final_prompt=f""" generate mysql sql query based on the below table schema:
-    {schema}
-    qustion- {user_input}
+    final_prompt=f""" Generate MYSQL SQL query based on the below table schema:
+
+    Table Schema-{schema}
+
+    Qustion- {user_input}
+
     Acceptance criteria- 
-    Only provide sql. dont add any text or character before and after sql script.
-    for joins first preference 'Left Join' and then 'Inner Join'.
-    Always perform Select operation.
-    Don't perform any Update, Insert and Delete operation if asked by user. Reply "This Operation can not be perform".
+    - Only provide SQL. 
+    - do NOT add any explanations, text or markdown before and after SQL script.
+    - for joins first preference 'Left Join' and 'Inner Join' second.
+    - Generate only read-only SELECT and WITH query.
+    - Never perform any UPDATE, INSERT and DELETE operation or any Data Changing operation.
 
     """
     # # print(final_prompt)
